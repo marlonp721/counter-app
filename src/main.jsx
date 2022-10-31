2e796edf1272c516112a793cb99d79b769bc6774
Marlon Peralta <marlonperalta@digital.gob.pe>
2022-10-31 01:12:12 -0500

    hola mundo

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+
+function App(){
+  return (<h1>Hola mundo</h1>);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
