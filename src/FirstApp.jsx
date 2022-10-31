d40c0bb24c7280990c7927016139fb5b4a6a2dce
Marlon Peralta <marlonperalta@digital.gob.pe>
2022-10-31 01:26:11 -0500

    first component

---
+++ src/FirstApp.jsx
@@ -0,0 +1,6 @@
+// export const App = () =>
+// {
+//     return (<div>Hola mundo</div>);
+// };
+
+export const FirstApp = () => <h1>Marlon</h1>;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {App} from './HelloWorld'
+import {FirstApp} from './FirstApp'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <FirstApp />
   </React.StrictMode>
 )
