4e835d09de28fe6b7e63372c50a524bdeb17913f
Marlon Peralta <marlonperalta@digital.gob.pe>
2022-11-05 17:06:11 -0500

    test toEqual typeof

---
+++ tests/base-pruebas/07-deses-arr.test.js
@@ -0,0 +1,18 @@
+import { retornaArreglo } from "../../src/base-pruebas/07-deses-arr";
+
+describe('Pruebas en 7-deses-arr', () => { 
+    test('debe de retornar un string y un numero', () => { 
+        const [ letters, numbers ] = retornaArreglo();
+        expect( letters ).toBe( 'ABC' );
+        expect( numbers ).toBe( 123 );
+
+        expect( typeof letters ).toBe( 'string' );
+        expect( typeof numbers ).toBe( 'number' );
+
+        // espera cualquier tipo de string
+
+        expect( letters ).toEqual( expect.any(String) );
+
+     });
+
+ });
