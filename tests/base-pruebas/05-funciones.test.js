abefb97a67d7fca0030d4d1e4a474810f8c42c28
Marlon Peralta <marlonperalta@digital.gob.pe>
2022-11-05 16:58:03 -0500

    test object toStrictEqual

---
+++ tests/base-pruebas/05-funciones.test.js
@@ -0,0 +1,33 @@
+import { getUser, getUsuarioActivo } from "../../src/base-pruebas/05-funciones";
+
+describe('Pruebas en 05-funciones', () => { 
+
+    test('getUser debe retornar un objeto', () => { 
+           const testUser ={
+                uid: 'ABC123',
+                username: 'El_Papi1502'
+           };
+
+           const user = getUser();
+
+        //    expect( testUser ).toStrictEqual( user );
+           expect( testUser ).toEqual( user );
+     });
+
+     test('getUsuarioActivo debe retornar un objeto', () => { 
+        const name = 'Marlon';
+        
+        const user = getUsuarioActivo( name )
+
+        expect( user ).toStrictEqual({
+            uid: 'ABC567',
+            username: name
+        });
+
+      });
+
+ });
+
+
+
+
